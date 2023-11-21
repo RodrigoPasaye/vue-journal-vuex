31aa9969d97462a19b97301dbbc4d267a7d19c22
RodrigoPasaye <cv6200@innovaccion.mx>
2023-11-20 18:56:57 -0600

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: "Dolore dolor reprehenderit eiusmod duis commodo commodo consectetur ea elit amet. Et nisi veniam elit consequat do nostrud consectetur velit labore Lorem fugiat nostrud excepteur elit. Ut do enim ad sunt Lorem qui nisi esse esse cupidatat. Quis adipisicing occaecat magna Lorem.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: "Dolor minim incididunt incididunt eu ex esse aute fugiat. Enim enim ullamco aute do quis minim anim irure adipisicing aute. Adipisicing nulla amet labore aliquip do cillum duis adipisicing veniam. Fugiat ipsum adipisicing amet ipsum laborum reprehenderit commodo eu. Nisi officia aliqua laborum ex in tempor deserunt mollit duis adipisicing deserunt dolor fugiat consequat. Tempor ad esse magna occaecat mollit commodo proident exercitation commodo duis labore sunt eu aliqua.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: "Reprehenderit eu adipisicing nostrud tempor. Esse nisi cupidatat minim ipsum sunt et adipisicing eiusmod anim excepteur laborum officia laborum. Officia fugiat reprehenderit ipsum aliqua non commodo excepteur proident cillum deserunt dolor sunt. Ipsum labore aliquip cillum eu nostrud cupidatat laborum duis quis nisi. Labore officia qui mollit cupidatat laborum voluptate enim amet cillum quis proident culpa ex eiusmod. Do veniam aliqua irure amet veniam id laborum quis Lorem velit.",
+      picture: null,
+    },
+  ],
+});
